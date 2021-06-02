b24ecfc3251f89a9aa643ed471ce34f7eaf2c834
Brightness999 <brightness0804@gmail.com>
2021-06-02 21:46:24 +0400

    add myproduct_template_v2

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -6,52 +6,54 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\DashboardSteps;
 use App\ImportList;
+use App\MyProducts;
 use App\Settings;
 use App\Order;
 use App\User;
+use App\Products;
 
 class AjaxController extends Controller
 {
-    
+
     public function index(Request $parameters)
     {
-        
-    	if($parameters['action'] == 'add_check'){
 
-    		if($row = DashboardSteps::find($parameters['id_user'])){
-    			if($parameters['step'] == 1)$row->step1 = $parameters['value'];
-    			if($parameters['step'] == 2)$row->step2 = $parameters['value'];
-    			if($parameters['step'] == 3)$row->step3 = $parameters['value'];
-    			if($parameters['step'] == 4)$row->step4 = $parameters['value'];
-    			if($parameters['step'] == 5)$row->step5 = $parameters['value'];
-    			if($parameters['step'] == 6)$row->step6 = $parameters['value'];
-    			$row->save();
-    		}else{
-    			$row = new DashboardSteps;
-    			$row->id = $parameters['id_user'];
-    			$row->step1 = 0;
-    			$row->step2 = 0;
-    			$row->step3 = 0;
-    			$row->step4 = 0;
-    			$row->step5 = 0;
-    			$row->step6 = 0;
-    			$row->save();
+        if ($parameters['action'] == 'add_check') {
+
+            if ($row = DashboardSteps::find($parameters['id_user'])) {
+                if ($parameters['step'] == 1) $row->step1 = $parameters['value'];
+                if ($parameters['step'] == 2) $row->step2 = $parameters['value'];
+                if ($parameters['step'] == 3) $row->step3 = $parameters['value'];
+                if ($parameters['step'] == 4) $row->step4 = $parameters['value'];
+                if ($parameters['step'] == 5) $row->step5 = $parameters['value'];
+                if ($parameters['step'] == 6) $row->step6 = $parameters['value'];
+                $row->save();
+            } else {
+                $row = new DashboardSteps;
+                $row->id = $parameters['id_user'];
+                $row->step1 = 0;
+                $row->step2 = 0;
+                $row->step3 = 0;
+                $row->step4 = 0;
+                $row->step5 = 0;
+                $row->step6 = 0;
+                $row->save();
 
 
-    			$row = DashboardSteps::find($parameters['id_user']);
-    			if($parameters['step'] == 1)$row->step1 = $parameters['value'];
-    			if($parameters['step'] == 2)$row->step2 = $parameters['value'];
-    			if($parameters['step'] == 3)$row->step3 = $parameters['value'];
-    			if($parameters['step'] == 4)$row->step4 = $parameters['value'];
-    			if($parameters['step'] == 5)$row->step5 = $parameters['value'];
-    			if($parameters['step'] == 6)$row->step6 = $parameters['value'];
-    			$row->save();
-    		}
+                $row = DashboardSteps::find($parameters['id_user']);
+                if ($parameters['step'] == 1) $row->step1 = $parameters['value'];
+                if ($parameters['step'] == 2) $row->step2 = $parameters['value'];
+                if ($parameters['step'] == 3) $row->step3 = $parameters['value'];
+                if ($parameters['step'] == 4) $row->step4 = $parameters['value'];
+                if ($parameters['step'] == 5) $row->step5 = $parameters['value'];
+                if ($parameters['step'] == 6) $row->step6 = $parameters['value'];
+                $row->save();
+            }
 
-    		echo json_encode(1);
-    	}
+            echo json_encode(1);
+        }
 
-        if($parameters['action'] == 'add_import_list'){
+        if ($parameters['action'] == 'add_import_list') {
 
             $row = new ImportList;
             $row->id_customer = Auth::user()->id;
@@ -61,25 +63,25 @@ class AjaxController extends Controller
             return json_encode($parameters['id_product']);
         }
 
-        if($parameters['action'] == 'delete_import_list'){
+        if ($parameters['action'] == 'delete_import_list') {
 
-			$this->authorize('plan_delete-product-import-list'); 
+            $this->authorize('plan_delete-product-import-list');
             $row = ImportList::find($parameters['id_import_list']);
             $row->delete();
 
             return json_encode(1);
-        } 
+        }
 
-        if($parameters['action'] == 'update_notes'){
+        if ($parameters['action'] == 'update_notes') {
             $row = Order::find($parameters['id_order']);
-            $row->notes = $row->notes.$parameters['notes'];
+            $row->notes = $row->notes . $parameters['notes'];
             $row->save();
 
             return json_encode(1);
-        }       
- 
+        }
 
-        if($parameters['action'] == 'save-user'){
+
+        if ($parameters['action'] == 'save-user') {
             $row = new User;
             $row->name = $parameters['user'];
             $row->email = $parameters['email'];
@@ -88,34 +90,51 @@ class AjaxController extends Controller
             $row->save();
 
             return json_encode(1);
-        }  
+        }
 
+        if ($parameters['action'] == 'my-products') {
+            $page_number = $parameters['page_number'];
+            $page_size = $parameters['page_size'];
+            $prods = Products::select('products.*', 'my_products.id_imp_product as id_my_product','my_products.id_shopify','my_products.id as id_my_products','my_products.profit')
+                ->join('import_list', 'import_list.id_product', '=', 'products.id')
+                ->join('my_products', 'my_products.id_imp_product', '=', 'import_list.id')
+                ->where('my_products.id_customer', Auth::user()->id)->whereNull('my_products.deleted_at')->orderBy('my_products.id', 'desc')->skip(($page_number - 1) * $page_size)->take($page_size)->get();
+            $total_count = MyProducts::count();
+            $search = new SearchController;
+            foreach ($prods as $product) {
+                $product['brand'] = $search->getAttributeByCode($product, 'brand');
+                if ($product->images != null && count(json_decode($product->images)) > 0)
+                    $product->image_url = env('URL_MAGENTO_IMAGES') . json_decode($product->images)[0]->file;
+            }
+            return json_encode([
+                'prods' => $prods,
+                'total_count' => $total_count,
+                'page_size' => $page_size,
+                'page_number' => $page_number
+            ]);
+        }
+    }
 
+    public function saveSettings(Request $request)
+    {
+        $request->validate([
+            'set8' => 'required|integer',
+        ]);
 
-
-	}
-	
-	public function saveSettings(Request $request){
-		//dd($request->all());
-		$request->validate([
-			'set8' => 'required|integer',
-		]);
-
-		$settings = Settings::where('id_merchant',Auth::user()->id)->first();
-		if($settings==null){
-			$settings = new Settings();
-			$settings->id_merchant = Auth::user()->id;
-		}
-		$settings->set1 = $request->set1=='true'?1:0;
-		$settings->set2 = $request->set2=='true'?1:0;
-		$settings->set3 = $request->set3=='true'?1:0;
-		$settings->set4 = $request->set4=='true'?1:0;
-		$settings->set5 = $request->set5=='true'?1:0;
-		$settings->set6 = $request->set6=='true'?1:0;
-		$settings->set7 = $request->set7=='true'?1:0;
-		$settings->set8 = $request->set8;
-		$settings->save();
-		return response()->json(['res'=>'ok']);
-	}
-    
+        $settings = Settings::where('id_merchant', Auth::user()->id)->first();
+        if ($settings == null) {
+            $settings = new Settings();
+            $settings->id_merchant = Auth::user()->id;
+        }
+        $settings->set1 = $request->set1 == 'true' ? 1 : 0;
+        $settings->set2 = $request->set2 == 'true' ? 1 : 0;
+        $settings->set3 = $request->set3 == 'true' ? 1 : 0;
+        $settings->set4 = $request->set4 == 'true' ? 1 : 0;
+        $settings->set5 = $request->set5 == 'true' ? 1 : 0;
+        $settings->set6 = $request->set6 == 'true' ? 1 : 0;
+        $settings->set7 = $request->set7 == 'true' ? 1 : 0;
+        $settings->set8 = $request->set8;
+        $settings->save();
+        return response()->json(['res' => 'ok']);
+    }
 }
--- app/Http/Controllers/MyProductsController.php
+++ app/Http/Controllers/MyProductsController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Libraries\Shopify\ShopifyAdminApi;
+use App\Jobs\ShopifyBulkDelete;
 use App\MyProducts;
 use Illuminate\Http\Request;
 use App\Products;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 
 class MyProductsController extends Controller
@@ -21,31 +22,82 @@ class MyProductsController extends Controller
     {
         $this->middleware('auth');
     }
-    
+
     public function index()
     {
-        $this->authorize('view-merchant-my-products'); 
-        $this->authorize('plan_view-my-products'); 
-        $prods = Products::select('products.*', 'my_products.id_imp_product as id_my_product','my_products.id_shopify','my_products.id as id_my_products','my_products.profit')
+        $this->authorize('view-merchant-my-products');
+        $this->authorize('plan_view-my-products');
+        $prods = Products::select('products.*', 'my_products.id_imp_product as id_my_product', 'my_products.id_shopify', 'my_products.id as id_my_products', 'my_products.profit')
             ->join('import_list', 'import_list.id_product', '=', 'products.id')
             ->join('my_products', 'my_products.id_imp_product', '=', 'import_list.id')
             ->where('my_products.id_customer', Auth::user()->id)->whereNull('my_products.deleted_at')->orderBy('my_products.id', 'desc')->paginate(10);
-
+        $total_count = MyProducts::count();
         $search = new SearchController;
         foreach ($prods as $product) {
             $product['brand'] = $search->getAttributeByCode($product, 'brand');
             if ($product->images != null && count(json_decode($product->images)) > 0)
                 $product->image_url = env('URL_MAGENTO_IMAGES') . json_decode($product->images)[0]->file;
         }
-        return view('my-products', ['prods' => $prods]);
+        return view('my-products_v2', ['prods' => $prods, 'total_count' => $total_count]);
     }
 
-    public function deleteProduct(MyProducts $product){
-        $result = ShopifyAdminApi::deleteProduct(Auth::User(),$product->id_shopify);
-        if(isset($result['HTTP_CODE'])&&$result['HTTP_CODE']==200){
-            $product->delete();
-        }else{
-            return response()->json(['message' => 'fail to delete product in shopify'], $result['HTTP_CODE']);
+    public function deleteProduct(Request $request)
+    {
+        $this->authorize('view-merchant-my-products');
+        $this->authorize('plan_view-my-products');
+        ShopifyBulkDelete::dispatchNow(Auth::User(), [$request->product_id]);
+        $result = MyProducts::where('id_shopify', $request->product_id)->get();
+        return response()->json([
+            'result' => count($result) == 0,
+            'product_id' => $request->product_id
+        ]);
+    }
+
+    public function deleteAllProduct(Request $request)
+    {
+        $this->authorize('view-merchant-my-products');
+        $this->authorize('plan_view-my-products');
+        $user_id = Auth::User()->id;
+        $rows = [];
+        foreach ($request->products as $product) {
+            $rows[] = [
+                'id' => $product['product_id'],
+                'user_id' => $user_id,
+                'payload' => $product['product_shopify_id'],
+                'action' => 'delete'
+            ];
         }
+        DB::table('temp_publish_products')->insert($rows);
+        $temp_product_ids = DB::table('temp_publish_products')
+            ->where('user_id', $user_id)
+            ->where('action', 'delete')
+            ->pluck('payload');
+        if (ShopifyBulkDelete::dispatch(Auth::User(), $temp_product_ids)) {
+            $result = 'ok';
+        }
+
+        return response()->json(['result' => $result]);
+    }
+
+    public function checkDeleteProducts(Request $request)
+    {
+        $this->authorize('view-merchant-my-products');
+        $this->authorize('plan_view-my-products');
+
+        $product_shopify_ids = $request->product_shopify_ids;
+        $result = DB::table('temp_publish_products')
+            ->whereIn('payload', $product_shopify_ids)->pluck('payload');
+        $data = [];
+        foreach ($product_shopify_ids as $product_shopify_id) {
+            $flag = true;
+            foreach ($result as $res) {
+                if ($product_shopify_id == $res) $flag = false;
+            }
+            if ($flag) $data[] = $product_shopify_id;
+        }
+
+        return response()->json([
+            'product_shopify_ids' => $data
+        ]);
     }
 }
